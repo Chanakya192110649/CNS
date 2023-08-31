ff96ff5701e3fc8c5e6078d582022d4bc2aaeff1
Chanakya192110649 <112469749+Chanakya192110649@users.noreply.github.com>
2023-08-31 09:45:52 +0530

    Update 8 DES.cpp

--- 8 DES.cpp
+++ 8 DES.cpp
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdint.h>
-static const int initial_permutation[64] = { /* Initial permutation table */ };
-static const int final_permutation[64] = { /* Final permutation table */ };
-static const int round_permutation[32] = { /* Round permutation table */ };
-static const int sbox[8][4][16] = { /* S-boxes */ };
-static const int key_schedule[16][48] = { /* Key schedule */ };
-void initialPermutation(uint64_t *block) {
-}
-void finalPermutation(uint64_t *block) {   
-}
-void roundFunction(uint32_t *left, uint32_t *right, const uint64_t round_key) {
-}
-void desEncrypt(const uint64_t plaintext, const uint64_t key, uint64_t *ciphertext) {  
-}
+void des_encrypt(uint64_t plainText, uint64_t key, uint64_t *cipherText);
+void print_binary(uint64_t num);
 int main() {
-    uint64_t plaintext = 0x0123456789ABCDEF; // 64-bit plaintext
-    uint64_t key = 0x133457799BBCDFF1;       // 64-bit key
-    uint64_t ciphertext;
-    desEncrypt(plaintext, key, &ciphertext);
-    printf("Plaintext:  %016llX\n", plaintext);
-    printf("Ciphertext: %016llX\n", ciphertext);
+    uint64_t plainText, key, cipherText;
+    printf("Enter the 64-bit plaintext: ");
+    scanf("%llx", &plainText);
+    printf("Enter the 64-bit encryption key: ");
+    scanf("%llx", &key);
+    des_encrypt(plainText, key, &cipherText);
+    printf("\nPlaintext: ");
+    print_binary(plainText);
+    printf("\nKey: ");
+    print_binary(key);
+    printf("\nCiphertext: ");
+    print_binary(cipherText);
     return 0;
+}
+
+void des_encrypt(uint64_t plainText, uint64_t key, uint64_t *cipherText) {
+    *cipherText = plainText;
+}
+void print_binary(uint64_t num) {
+    for (int i = 63; i >= 0; i--) {
+        uint64_t bit = (num >> i) & 1;
+        printf("%llu", bit);
+        if (i % 8 == 0)
+            printf(" ");
+    }
+    printf("\n");
 }
